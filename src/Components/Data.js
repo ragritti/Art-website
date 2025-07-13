7c8b77b963138127a8782dd16324507c959a5a56
ragritti <116258047+ragritti@users.noreply.github.com>
2025-07-14 00:15:03 +0530

    added bear painting

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -5,53 +5,54 @@ export const masterData = {
       title: 'Water Colours',
       subtitle: 'Hand-drawn masterpieces',
       data: [
-        { id: 1, title: 'W001', price: 5000, desc:'8*8',imageUrl: '/images/WaterColours/015.jpg', description: 'Handmade Charcoal Art' },
-        { id: 17, title: 'W002', price: 35000, desc:'22*30',imageUrl: '/images/WaterColours/023.jpeg', description: 'Handmade Charcoal Art' },
-        { id: 16, title: 'W003', price: 10000, desc:'11.69*8.27',imageUrl: '/images/WaterColours/022.jpeg', description: 'Handmade Charcoal Art' },
-        { id: 4, title: 'W004', price: 4000, desc:'8*8',imageUrl: '/images/WaterColours/016.jpg', description: 'Handmade Charcoal Art' },
-        { id: 5, title: 'W005', price: 10000, desc:'8.3*11.7',imageUrl: '/images/WaterColours/005.jpg', description: 'Handmade Charcoal Art' },
-        { id: 6, title: 'W006', price: 5000, desc:'8*8',imageUrl: '/images/WaterColours/014.jpg', description: 'Handmade Charcoal Art' },
-        { id: 7, title: 'W007', price: 14000, desc:'16*12',imageUrl: '/images/WaterColours/002.jpg', description: 'Handmade Charcoal Art' },
-        { id: 8, title: 'W008', price: 8000, desc:'9*12',imageUrl: '/images/WaterColours/012.jpg', description: 'Handmade Charcoal Art' },
-        { id: 9, title: 'W009', price: 12000, desc:'11*14',imageUrl: '/images/WaterColours/008.jpg', description: 'Handmade Charcoal Art' },
-        { id: 10, title:'W010', price: 10000,desc:'9*12', imageUrl: '/images/WaterColours/007.jpg', description: 'Handmade Charcoal Art' },
-        { id: 11, title:'W011', price: 4000,desc:'8.3*11.7', imageUrl: '/images/WaterColours/003.jpg', description: 'Handmade Charcoal Art' },
-        { id: 12, title:'W012', price: 12000,desc:'16*12', imageUrl: '/images/WaterColours/011.jpg', description: 'Handmade Charcoal Art' },
-        { id: 13, title:'W013', price: 8000,desc:'9*12', imageUrl: '/images/WaterColours/004.jpg', description: 'Handmade Charcoal Art' },
-        { id: 14, title:'W014', price: 12000,desc:'8.3*11.7', imageUrl: '/images/WaterColours/018.jpg', description: 'Handmade Charcoal Art' },
-        { id: 15, title: 'W015', price: 8000, desc:'9*12',imageUrl: '/images/WaterColours/017.jpg', description: 'Handmade Charcoal Art' },
-        { id: 3, title: 'W016', price: 12000,desc:'9*12', imageUrl: '/images/WaterColours/019.jpg', description: 'Handmade Charcoal Art' },
-        { id: 16, title: 'W017', price: 14000, desc:'12*16',imageUrl: '/images/WaterColours/021.jpeg', description: 'Handmade Charcoal Art' },
-        { id: 2, title: 'W018', price: 10000, desc:'8.3*11.7',imageUrl: '/images/WaterColours/020.jpeg', description: 'Handmade Charcoal Art' },
+        { id: 1, title: 'W001', price: 5000, desc: '8*8', imageUrl: '/images/WaterColours/015.jpg', description: 'Handmade Charcoal Art' },
+        { id: 17, title: 'W002', price: 35000, desc: '22*30', imageUrl: '/images/WaterColours/023.jpeg', description: 'Handmade Charcoal Art' },
+        { id: 18, title: 'W003', price: 14000, desc: '16*12', imageUrl: '/images/WaterColours/024.jpeg', description: 'Handmade Charcoal Art' },
+        { id: 4, title: 'W004', price: 4000, desc: '8*8', imageUrl: '/images/WaterColours/016.jpg', description: 'Handmade Charcoal Art' },
+        { id: 5, title: 'W005', price: 10000, desc: '8.3*11.7', imageUrl: '/images/WaterColours/005.jpg', description: 'Handmade Charcoal Art' },
+        { id: 6, title: 'W006', price: 5000, desc: '8*8', imageUrl: '/images/WaterColours/014.jpg', description: 'Handmade Charcoal Art' },
+        { id: 7, title: 'W007', price: 14000, desc: '16*12', imageUrl: '/images/WaterColours/002.jpg', description: 'Handmade Charcoal Art' },
+        { id: 8, title: 'W008', price: 8000, desc: '9*12', imageUrl: '/images/WaterColours/012.jpg', description: 'Handmade Charcoal Art' },
+        { id: 9, title: 'W009', price: 12000, desc: '11*14', imageUrl: '/images/WaterColours/008.jpg', description: 'Handmade Charcoal Art' },
+        { id: 10, title: 'W010', price: 10000, desc: '9*12', imageUrl: '/images/WaterColours/007.jpg', description: 'Handmade Charcoal Art' },
+        { id: 11, title: 'W011', price: 4000, desc: '8.3*11.7', imageUrl: '/images/WaterColours/003.jpg', description: 'Handmade Charcoal Art' },
+        { id: 12, title: 'W012', price: 12000, desc: '16*12', imageUrl: '/images/WaterColours/011.jpg', description: 'Handmade Charcoal Art' },
+        { id: 13, title: 'W013', price: 8000, desc: '9*12', imageUrl: '/images/WaterColours/004.jpg', description: 'Handmade Charcoal Art' },
+        { id: 14, title: 'W014', price: 12000, desc: '8.3*11.7', imageUrl: '/images/WaterColours/018.jpg', description: 'Handmade Charcoal Art' },
+        { id: 15, title: 'W015', price: 8000, desc: '9*12', imageUrl: '/images/WaterColours/017.jpg', description: 'Handmade Charcoal Art' },
+        { id: 3, title: 'W016', price: 12000, desc: '9*12', imageUrl: '/images/WaterColours/019.jpg', description: 'Handmade Charcoal Art' },
+        { id: 16, title: 'W017', price: 14000, desc: '12*16', imageUrl: '/images/WaterColours/021.jpeg', description: 'Handmade Charcoal Art' },
+        { id: 2, title: 'W018', price: 10000, desc: '8.3*11.7', imageUrl: '/images/WaterColours/020.jpeg', description: 'Handmade Charcoal Art' },
+        { id: 16, title: 'W019', price: 10000, desc: '11.69*8.27', imageUrl: '/images/WaterColours/022.jpeg', description: 'Handmade Charcoal Art' },
 
       ]
-      
+
     },
     {
       id: 2,
       title: 'Flowers (Water Colours)',
       subtitle: 'Hand-drawn masterpieces',
       data: [
-        { id: 1, title: 'WF001', price: 5000,desc:'8*8', imageUrl: '/images/WaterColours/WF01.jpg', description: 'Handmade Charcoal Art' },
-        { id: 2, title: 'WF002', price: 4000,desc:'5.8*8.3', imageUrl: '/images/WaterColours/WF02.jpg', description: 'Handmade Charcoal Art' },
-        { id: 3, title: 'WF003', price: 10000,desc:'9*12', imageUrl: '/images/WaterColours/WF03.jpg', description: 'Handmade Charcoal Art' },
-        { id: 4, title: 'WF004', price: 5000,desc:'8*8', imageUrl: '/images/WaterColours/WF04.jpg', description: 'Handmade Charcoal Art' },
-        { id: 5, title: 'WF005', price: 10000,desc:'9*12', imageUrl: '/images/WaterColours/WF05.jpg', description: 'Handmade Charcoal Art' },
+        { id: 1, title: 'WF001', price: 5000, desc: '8*8', imageUrl: '/images/WaterColours/WF01.jpg', description: 'Handmade Charcoal Art' },
+        { id: 2, title: 'WF002', price: 4000, desc: '5.8*8.3', imageUrl: '/images/WaterColours/WF02.jpg', description: 'Handmade Charcoal Art' },
+        { id: 3, title: 'WF003', price: 10000, desc: '9*12', imageUrl: '/images/WaterColours/WF03.jpg', description: 'Handmade Charcoal Art' },
+        { id: 4, title: 'WF004', price: 5000, desc: '8*8', imageUrl: '/images/WaterColours/WF04.jpg', description: 'Handmade Charcoal Art' },
+        { id: 5, title: 'WF005', price: 10000, desc: '9*12', imageUrl: '/images/WaterColours/WF05.jpg', description: 'Handmade Charcoal Art' },
 
       ]
-      
+
     },
     {
       id: 3,
       title: 'Oil Paintings',
       subtitle: 'Precision in every stroke',
       data: [
-        { id: 6, title: 'OP001', price: 25000,desc:'18*24',  imageUrl: '/images/Oilpaintings/op_6.jpeg', description: 'Expressive Acrylic Art' },
-        { id: 5, title: 'OP002', price: 50000,desc:'24*36',  imageUrl: '/images/Oilpaintings/op_5.jpeg', description: 'Detailed Graphite Work' },
-        { id: 1, title: 'OP003', price: 15000,desc:'15*22',  imageUrl: '/images/Oilpaintings/op_1.jpg', description: 'Detailed Graphite Work' },
-        { id: 2, title: 'OP004', price: 25000,desc:'18*24',  imageUrl: '/images/Oilpaintings/op_2.jpg', description: 'Detailed Graphite Work' },
-        { id: 3, title: 'OP005', price: 12000,desc:'9*12', imageUrl:'/images/Oilpaintings/op_3.jpg', description: 'Detailed Graphite Work' },
-        { id: 4, title: 'OP006', price: 8000,desc:'12',  imageUrl: '/images/Oilpaintings/op_4.jpg', description: 'Expressive Acrylic Art' },
+        { id: 6, title: 'OP001', price: 25000, desc: '18*24', imageUrl: '/images/Oilpaintings/op_6.jpeg', description: 'Expressive Acrylic Art' },
+        { id: 5, title: 'OP002', price: 50000, desc: '24*36', imageUrl: '/images/Oilpaintings/op_5.jpeg', description: 'Detailed Graphite Work' },
+        { id: 1, title: 'OP003', price: 15000, desc: '15*22', imageUrl: '/images/Oilpaintings/op_1.jpg', description: 'Detailed Graphite Work' },
+        { id: 2, title: 'OP004', price: 25000, desc: '18*24', imageUrl: '/images/Oilpaintings/op_2.jpg', description: 'Detailed Graphite Work' },
+        { id: 3, title: 'OP005', price: 12000, desc: '9*12', imageUrl: '/images/Oilpaintings/op_3.jpg', description: 'Detailed Graphite Work' },
+        { id: 4, title: 'OP006', price: 8000, desc: '12', imageUrl: '/images/Oilpaintings/op_4.jpg', description: 'Expressive Acrylic Art' },
 
 
       ]
@@ -61,36 +62,36 @@ export const masterData = {
       title: 'Acrylic Art',
       subtitle: 'Bold and vibrant colors',
       data: [
-        { id: 1, title: 'A001', price: 22000,desc:'24*18',  imageUrl:  '/images/Acrylic/ac_4.jpeg', description: 'Expressive Acrylic Art' },
-        { id: 2, title: 'A002', price: 10000,desc:'13.3*17.3',  imageUrl:  '/images/Acrylic/ac_1.jpg', description: 'Expressive Acrylic Art' },
-        { id: 3, title: 'A003', price: 4000,desc:'11.8*11.8', imageUrl:'/images/Acrylic/ac_2.jpg', description: 'Expressive Acrylic Art' },
-        { id: 4, title: 'A004', price: 2000,desc:'4*8', imageUrl:'/images/Acrylic/ac_3.jpg', description: 'Expressive Acrylic Art' }, 
-   ]
-   },
+        { id: 1, title: 'A001', price: 22000, desc: '24*18', imageUrl: '/images/Acrylic/ac_4.jpeg', description: 'Expressive Acrylic Art' },
+        { id: 2, title: 'A002', price: 10000, desc: '13.3*17.3', imageUrl: '/images/Acrylic/ac_1.jpg', description: 'Expressive Acrylic Art' },
+        { id: 3, title: 'A003', price: 4000, desc: '11.8*11.8', imageUrl: '/images/Acrylic/ac_2.jpg', description: 'Expressive Acrylic Art' },
+        { id: 4, title: 'A004', price: 2000, desc: '4*8', imageUrl: '/images/Acrylic/ac_3.jpg', description: 'Expressive Acrylic Art' },
+      ]
+    },
     {
       id: 4,
       title: 'Graphite/Charcoal Art',
       subtitle: 'Soft and transparent layers',
       data: [
-        { id: 4, title: 'GC001', price: 30000,desc:'24*16.5', imageUrl: '/images/Graphite/gc_4.jpeg', description: 'Serene Watercolour Art' },
-        { id: 1, title: 'GC002', price: 5000,desc:'11.7*16.5', imageUrl: '/images/Graphite/gc_1.jpg', description: 'Serene Watercolour Art' },
-        { id: 2, title: 'GC003', price: 12000,desc:'9*12', imageUrl: '/images/Graphite/gc_2.jpg', description: 'Serene Watercolour Art' },
-        { id: 3, title: 'GC004', price: 15000,desc:'11.7*16.5', imageUrl: '/images/Graphite/gc_3.jpg', description: 'Serene Watercolour Art' },
+        { id: 4, title: 'GC001', price: 30000, desc: '24*16.5', imageUrl: '/images/Graphite/gc_4.jpeg', description: 'Serene Watercolour Art' },
+        { id: 1, title: 'GC002', price: 5000, desc: '11.7*16.5', imageUrl: '/images/Graphite/gc_1.jpg', description: 'Serene Watercolour Art' },
+        { id: 2, title: 'GC003', price: 12000, desc: '9*12', imageUrl: '/images/Graphite/gc_2.jpg', description: 'Serene Watercolour Art' },
+        { id: 3, title: 'GC004', price: 15000, desc: '11.7*16.5', imageUrl: '/images/Graphite/gc_3.jpg', description: 'Serene Watercolour Art' },
       ]
     },
     {
       id: 5,
       title: 'Miscellaneous',
       subtitle: 'Rich and textural compositions',
-      data: 
-      [
-        { id: 5, title: 'M001', price: 10000,desc:'9.5*12', imageUrl: '/images/Miscellaneous/o_5.jpeg', description: 'Detailed Oil Painting' },
-        { id: 1, title: 'M002', price: 12000,desc:'8.3*11.7', imageUrl: '/images/Miscellaneous/o_1.jpg', description: 'Detailed Oil Painting' },
-        { id: 2, title: 'M003', price: 10000,desc:'9.5*12', imageUrl: '/images/Miscellaneous/o_2.jpg', description: 'Detailed Oil Painting' },
-        { id: 3, title: 'M004', price: 12000,desc:'11.7*16.5', imageUrl: '/images/Miscellaneous/o_3.jpg', description: 'Detailed Oil Painting' },
-        { id: 4, title: 'M005', price: 10000,desc:'8.3*11.7', imageUrl: '/images/Miscellaneous/o_4.jpg', description: 'Detailed Oil Painting' },
+      data:
+        [
+          { id: 5, title: 'M001', price: 10000, desc: '9.5*12', imageUrl: '/images/Miscellaneous/o_5.jpeg', description: 'Detailed Oil Painting' },
+          { id: 1, title: 'M002', price: 12000, desc: '8.3*11.7', imageUrl: '/images/Miscellaneous/o_1.jpg', description: 'Detailed Oil Painting' },
+          { id: 2, title: 'M003', price: 10000, desc: '9.5*12', imageUrl: '/images/Miscellaneous/o_2.jpg', description: 'Detailed Oil Painting' },
+          { id: 3, title: 'M004', price: 12000, desc: '11.7*16.5', imageUrl: '/images/Miscellaneous/o_3.jpg', description: 'Detailed Oil Painting' },
+          { id: 4, title: 'M005', price: 10000, desc: '8.3*11.7', imageUrl: '/images/Miscellaneous/o_4.jpg', description: 'Detailed Oil Painting' },
 
-      ]
+        ]
     },
   ]
 };
