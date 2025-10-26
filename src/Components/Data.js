794e93f182d21670e882a421bf031514f4a88a68
ragritti <116258047+ragritti@users.noreply.github.com>
2025-10-26 13:47:16 +0530

    added elephant painting

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -6,8 +6,8 @@ export const masterData = {
       subtitle: 'Hand-drawn masterpieces',
       data: [
         { id: 1, title: 'W001', price: 18000, desc: '24*18', imageUrl: '/images/WaterColours/026.jpeg', description: 'Handmade Charcoal Art' },
-        { id: 17, title: 'W002', price: 35000, desc: '22*30', imageUrl: '/images/WaterColours/023.jpeg', description: 'Handmade Charcoal Art' },
-        { id: 18, title: 'W003', price: 14000, desc: '16*12', imageUrl: '/images/WaterColours/024.jpeg', description: 'Handmade Charcoal Art' },
+        { id: 19, title: 'W002', price: 35000, desc: '22*30', imageUrl: '/images/WaterColours/023.jpeg', description: 'Handmade Charcoal Art' },
+        { id: 20, title: 'W003', price: 16000, desc: '12*18', imageUrl: '/images/WaterColours/027.jpeg', description: 'Handmade Charcoal Art' },
         { id: 17, title: 'W004', price: 22000, desc: '18*14', imageUrl: '/images/WaterColours/025.jpeg', description: 'Handmade Charcoal Art' },
         { id: 5, title: 'W005', price: 10000, desc: '8.3*11.7', imageUrl: '/images/WaterColours/005.jpg', description: 'Handmade Charcoal Art' },
         { id: 6, title: 'W006', price: 5000, desc: '8*8', imageUrl: '/images/WaterColours/014.jpg', description: 'Handmade Charcoal Art' },
@@ -26,6 +26,8 @@ export const masterData = {
         { id: 16, title: 'W019', price: 10000, desc: '11.69*8.27', imageUrl: '/images/WaterColours/022.jpeg', description: 'Handmade Charcoal Art' },
         { id: 4, title: 'W020', price: 4000, desc: '8*8', imageUrl: '/images/WaterColours/016.jpg', description: 'Handmade Charcoal Art' },
         { id: 1, title: 'W021', price: 5000, desc: '8*8', imageUrl: '/images/WaterColours/015.jpg', description: 'Handmade Charcoal Art' },
+        { id: 18, title: 'W022', price: 14000, desc: '16*12', imageUrl: '/images/WaterColours/024.jpeg', description: 'Handmade Charcoal Art' },
+
       ]
 
     },
